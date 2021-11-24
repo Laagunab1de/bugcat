6d5f2c0705fd42703ce5f981fae0028cab1cafc3
andreynidrih <andreynidrih@gmail.com>
2021-11-24 18:00:16 +1000

    Add project files.

---
+++ bugcat/Class2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koshka
+{
+    internal class CatSmartHouse
+    {
+
+        List<Cat> cats = new List<Cat>();
+        public int foodResourse;
+
+        public int FoodResoure
+        {
+            get { return foodResourse; }
+            set { foodResourse = 1000; }
+        }
+        public void addcat(object sender, Cat cat)
+        {
+
+            var Cat = (Cat)sender;
+            if (cat.hungrystatus <= 20 && foodResourse > 0)
+            {
+
+                byte needFood = (byte)(100 - cat.hungrystatus);
+                if (foodResourse > needFood)
+                    foodResourse -= needFood;
+                else
+                {
+                    needFood = (byte)foodResourse;
+                    foodResourse = 0;
+                }
+                cat.Feed(needFood);
+                Console.WriteLine($"Покормлена кошка: {cat.Name}\nОстаток еды в вольере: {foodResourse}");
+            }
+        }
+    }
+}
---
+++ bugcat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Барсик", new DateTime(2015, 8, 22));
+            Cat cat1 = new Cat("Маруся", new DateTime(2010, 4, 15));
+            cat.makenoise();
+            Console.WriteLine($"знакомьтесь, {cat.Name}, ему уже {cat.getage()} лет");
+            cat.Hungrystatus = 150;
+            Console.WriteLine("Hungry = " + cat.Hungrystatus + "\n");
+            cat.getstatus();
+            cat.HungryStatusChanged += Cat_HungryStatusChanged;
+            cat1.makenoise();
+            Console.WriteLine($"знакомьтесь, {cat1.Name}, ей уже {cat1.getage()} лет");
+            cat1.Hungrystatus = 150;
+            Console.WriteLine("Hungry = " + cat.Hungrystatus + "\n");
+            cat1.getstatus();
+            Console.ReadLine();
+            cat1.HungryStatusChanged += Cat_HungryStatusChanged;
+
+        }
+
+        private static void Cat_HungryStatusChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
